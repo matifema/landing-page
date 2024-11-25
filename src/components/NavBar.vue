a0940bad3da90e5408687f1c3efbf5f747cbd6c2
marcoferri42 <marcoferri462@gmail.com>
2024-11-25 16:56:53 +0100

    mmm

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,7 +20,7 @@ export default {
 <style scoped>
 @font-face {
   font-family: "AudioNugget";
-  src: url("src/assets/fonts/audionugget-webfont.woff");
+  src: url("src/assets/fonts/audionugget-webfont.woff") format("woff");
 }
 .side-nav {
   font-family: AudioNugget;

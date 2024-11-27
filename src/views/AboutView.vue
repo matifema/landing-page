2d043a844c7f1feb92b670a34240f47677e8196d
marcoferri42 <marcoferri462@gmail.com>
2024-11-27 21:57:51 +0100

    fixed some package vulnerabilities

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,7 +7,7 @@ import Resume from '@/components/Resume.vue';
     <h2 style="color: var(--accent);">about me</h2>
     <p class="text" style="text-align: center;">
       dev who's interested in cybersec, computing, design and building cool things. <br>
-      also passionate about astrophysics, rock climbing, dogs and biomimetics.
+      also passionate about astrophysics, dogs and biomimetics.
     </p>
     <hr>
     <!--<Resume/>-->
